1d99e36e8ca3eca624c280e2b87e9c78ce7c6b44
Biniyam <binibelaygom@gmail.com>
2025-04-09 22:22:27 +0300

    recommit

--- backend/src/prisma/migrations/20250329090832_add_product_details_fields/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "products" ADD COLUMN     "originalPrice" DECIMAL(10,2),
-ADD COLUMN     "rating" DOUBLE PRECISION DEFAULT 0,
-ADD COLUMN     "reviewCount" INTEGER DEFAULT 0,
-ADD COLUMN     "sellerLocation" TEXT,
-ADD COLUMN     "sellerName" TEXT,
-ADD COLUMN     "unitsSold" INTEGER DEFAULT 0;
